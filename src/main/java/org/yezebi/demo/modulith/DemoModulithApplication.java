7fde2271cd7a076df294aeb0229621a7995d3241
Taniel Rémi <taniel.remi@gmail.com>
2025-10-04 09:17:27 +0200

    ➕ add spring modulith

--- src/main/java/org/yezebi/demo/modulith/DemoModulithApplication.java
+++ src/main/java/org/yezebi/demo/modulith/DemoModulithApplication.java
@@ -3,7 +3,9 @@ package org.yezebi.demo.modulith;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.modulith.Modulithic;
 
+@Modulithic
 @SpringBootApplication
 @ConfigurationPropertiesScan("org.yezebi.demo.modulith")
 public class DemoModulithApplication {
--- src/test/java/org/yezebi/demo/modulith/ModuleArchitectureTest.java
+++ src/test/java/org/yezebi/demo/modulith/ModuleArchitectureTest.java
@@ -1,3 +1,24 @@
 package org.yezebi.demo.modulith;
 
-class ModuleArchitectureTest {}
+import org.junit.jupiter.api.Test;
+import org.springframework.modulith.core.ApplicationModules;
+import org.springframework.modulith.docs.Documenter;
+
+class ModuleArchitectureTest {
+  final ApplicationModules modules = ApplicationModules.of(DemoModulithApplication.class);
+
+  @Test
+  public void verifyModuleBeans() {
+    modules.forEach(System.out::println);
+  }
+
+  @Test
+  public void verifyModularArchitecture() {
+    modules.verify();
+  }
+
+  @Test
+  public void createModuleDocumentation() {
+    new Documenter(modules).writeModulesAsPlantUml().writeIndividualModulesAsPlantUml();
+  }
+}
